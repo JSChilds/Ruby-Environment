cae05934a01a5f01a7b1890917ff81f600c9f4c0
Tech-A40 <Tech-A40@Tech-A40s-MacBook-Air.local>
2017-08-09 17:12:45 +0100

    Pushing to test web hooks for Jenkins

--- environment/provision.sh
+++ environment/provision.sh
@@ -4,6 +4,14 @@ sudo apt-add-repository ppa:brightbox/ruby-ng
 sudo apt-get update -y
 sudo apt-get install ruby2.4 ruby2.4-dev -y
 
+
+sudo apt-get install -y dirmngr gnupg
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
+sudo apt-get install -y apt-transport-https ca-certificates
+sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger xenial main > /etc/apt/sources.list.d/passenger.list'
+sudo apt-get update
+sudo apt-get install -y nginx-extras passenger
+
 sudo gem install bundler
 
 sudo apt-get install rails -y
