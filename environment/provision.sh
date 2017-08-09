9bb2faa2cbfe8fd4e7063defe7ff0f8d53116c81
Tech-A40 <Tech-A40@Tech-A40s-MacBook-Air.local>
2017-08-09 10:22:59 +0100

    Added reverse proxying to nginx via provisioning

--- environment/provision.sh
+++ environment/provision.sh
@@ -8,3 +8,7 @@ sudo gem install bundler
 
 sudo apt-get install rails -y
 sudo apt-get install nginx -y 
+
+sudo rm /etc/nginx/sites-available/default
+sudo cp ~/app/environment/templates/default /etc/nginx/sites-available/default
+sudo service nginx reload
