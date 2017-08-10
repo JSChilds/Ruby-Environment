d9944e9bf6fbfd7ce77443ecfdc8bb363e2b2e00
Tech-A40 <Tech-A40@Tech-A40s-MacBook-Air.local>
2017-08-10 11:36:04 +0100

    Added auto no to nginx and user permissions for app directory

--- environment/provision.sh
+++ environment/provision.sh
@@ -10,17 +10,19 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CA
 sudo apt-get install -y apt-transport-https ca-certificates
 sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger xenial main > /etc/apt/sources.list.d/passenger.list'
 sudo apt-get update
-sudo apt-get install -y nginx-extras passenger
+yes N | sudo apt-get install -y nginx-extras passenger
 
 sudo gem install bundler
 
 sudo apt-get install rails -y
-sudo apt-get install nginx -y 
+# sudo apt-get install nginx -y 
 
 sudo rm /etc/nginx/sites-available/default
 sudo cp ~/app/environment/templates/default /etc/nginx/sites-available/default
 
 sudo rm /etc/nginx/nginx.conf 
 sudo cp ~/app/environment/templates/nginx.conf /etc/nginx/nginx.conf
+
+chmod +x ~/app
 
 sudo service nginx reload
